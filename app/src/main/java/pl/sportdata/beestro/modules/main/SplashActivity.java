791d8258d8a3e5310c37335793404ca9f68d24cd
Krzysztof Szałach <esh0.pl@gmail.com>
2017-08-08 08:45:38 +0200

    [BEE-2] Remove 1 month date protection from app after implementing [BEE-1] (#4)

--- app/src/main/java/pl/sportdata/beestro/modules/main/SplashActivity.java
+++ app/src/main/java/pl/sportdata/beestro/modules/main/SplashActivity.java
@@ -4,12 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
 
 import pl.sportdata.beestro.BeestroApplication;
 import pl.sportdata.beestro.R;
 import pl.sportdata.beestro.modules.credentials.LoginActivityImpl;
-import pl.sportdata.beestro.utils.LicenseValidator;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -20,20 +18,16 @@ public class SplashActivity extends AppCompatActivity {
         Runnable splashDelayed = new Runnable() {
             @Override
             public void run() {
-                if (!isFinishing()) {
-                    Intent intent = null;
-                    if (!LicenseValidator.isLicenseValid()) {
-                        Toast.makeText(SplashActivity.this, R.string.license_expired, Toast.LENGTH_LONG).show();
-                    } else if (((BeestroApplication) getApplication()).getLoggedUser() != null) {
-                        intent = new Intent(SplashActivity.this, MainActivityImpl.class);
-                    } else {
-                        intent = new Intent(SplashActivity.this, LoginActivityImpl.class);
-                    }
+                Intent intent;
+                if (((BeestroApplication) getApplication()).getLoggedUser() != null) {
+                    intent = new Intent(SplashActivity.this, MainActivityImpl.class);
+                } else {
+                    intent = new Intent(SplashActivity.this, LoginActivityImpl.class);
+                }
 
-                    if (intent != null) {
-                        startActivity(intent);
-                        overridePendingTransition(0, 0);
-                    }
+                if (!isFinishing()) {
+                    startActivity(intent);
+                    overridePendingTransition(0, 0);
                     finish();
                 }
             }
